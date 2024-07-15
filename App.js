0dff58ecbf9a1779ecdd483bc9f6f28d3cae372f
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-15 21:29:45 +0530

    added style

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello World form React");
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World form React"
+);
+console.log(heading) //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
