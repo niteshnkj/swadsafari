783108bc0fb6a8311d480e02725bebd324064a69
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-15 21:54:59 +0530

    inception

--- App.js
+++ App.js
@@ -16,12 +16,12 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("div", { id: "h1" }, "I'm an h1 tag"),
-    React.createElement("div", { id: "h2" }, "I'm an h2 tag"),
+    React.createElement("h1", { id: "h1" }, "I'm an h1 tag"),
+    React.createElement("h2", { id: "h2" }, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("div", { id: "h1" }, "I'm an h1 tag"),
-    React.createElement("div", { id: "h2" }, "I'm an h2 tag"),
+    React.createElement("h1", { id: "h1" }, "I'm an h1 tag"),
+    React.createElement("h2", { id: "h2" }, "I'm an h2 tag"),
   ]),
 ]);
 console.log(parent);
