2cba33d02e409ff436c289268db19cba39802e59
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-15 21:35:42 +0530

    nested child

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
+/**
+ * <div id="parent">
+ * <div id="children">
+ * <h1>I'm children</h1>
+ * </div>
+ * </div>
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "h1" }, "I'm an h1 tag"),
+  React.createElement("div", { id: "h2" }, "I'm an h2 tag"),
+]);
+console.log(parent);
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello World form React"
 );
-console.log(heading) //object
+// console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
