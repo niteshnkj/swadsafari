183007a757b8cdcf7bfbcb16d0da6fdc7638ffe7
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-16 23:12:23 +0530

    planning && res card

--- App.js
+++ App.js
@@ -1,24 +1,101 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// React Element=>jsx
-const title = <h1>namaste React using jsx</h1>;
 
-// React Component
-// Class Based Component - OLD
-// Functional COmponent - NEW
-
-// react Functional component is just a normal js function that returns some piece of jsx
-// component composition when we use components inside a component
-const HeadingComponent = () => {
+/**
+ * Header
+ * -Logo
+ * -Nav Items
+ * Body
+ * - search
+ * - restaurantContainer
+ * - restaurantCard
+ * - name of res, Start Rating,cuisins,deliverytime
+ * - Footer
+ *   - Copyright
+ *   - Links
+ *   - Address
+ *    - contact
+ */
+const Header = () => {
   return (
-    <>
-      {title}
-      <h1>Namaste React Functional Component</h1>
-      {/* <Title /> */}
-    </>
+    <div className="header">
+      <div className="Logo-container">
+        <img src="" alt="Logo" className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <li>User</li>
+        </ul>
+      </div>
+    </div>
   );
 };
-// const HeadingComponent2 = () => <h1>Namaste React Functional Component</h1>;
+const RestrauntCard = () => {
+  return (
+    <div className="res-card " style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Chicken_briyani.jpg"
+        alt="res-logo"
+        className="res-logo"
+      />
+      <h3>Meghna Foods</h3>
+      <h4>Briyani,North Indian,Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
