7700349a25b7bf3c4c4a51010f2bcc2f16a112a3
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-16 17:56:54 +0530

    componet vs element && component composition

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//React.createElement = >  ReactElement-js Object => HTMLELEMENT(render)
-// const heading = React.createElement("h1", {}, "Namaste Bhai");
-// console.log(heading);
-
-// JSX - HTML like or XML-like syntax
-// JSX (transpiled before it reaches the JS Engine) - Parcel - Babel
-// jSX =>Babel transpiles it to React.createElement => ReactElement-JS Object => HtmlElement(render)
+// React Element=>jsx
 const Heading = <h1>namaste React using jsx</h1>;
 console.log(Heading);
+
+// React Component
+// Class Based Component - OLD
+// Functional COmponent - NEW
+
+// react Functional component is just a normal js function that returns some piece of jsx
+// component composition when we use components inside a component
+const HeadingComponent = () => {
+  return <h1>Namaste React Functional Component</h1>;
+};
+// const HeadingComponent2 = () => <h1>Namaste React Functional Component</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(Heading);
