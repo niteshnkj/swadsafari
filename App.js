546089d67b257493158ff3ad5c362729094ff05d
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-16 19:28:48 +0530

    jsx && component

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // React Element=>jsx
-const Heading = <h1>namaste React using jsx</h1>;
-console.log(Heading);
+const title = <h1>namaste React using jsx</h1>;
 
 // React Component
 // Class Based Component - OLD
@@ -11,9 +10,15 @@ console.log(Heading);
 // react Functional component is just a normal js function that returns some piece of jsx
 // component composition when we use components inside a component
 const HeadingComponent = () => {
-  return <h1>Namaste React Functional Component</h1>;
+  return (
+    <>
+      {title}
+      <h1>Namaste React Functional Component</h1>
+      {/* <Title /> */}
+    </>
+  );
 };
 // const HeadingComponent2 = () => <h1>Namaste React Functional Component</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Heading);
+root.render(<HeadingComponent />);
